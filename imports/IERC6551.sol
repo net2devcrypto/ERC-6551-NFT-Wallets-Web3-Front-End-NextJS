f2094201acb7019d107dcc5e81745af93f7fc858
Net2Dev <info@a3b.io>
2024-01-28 17:05:38 -0800

    Add files via upload

---
+++ imports/IERC6551.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IERC6551Registry {
+
+    event AccountCreated(
+        address account,
+        address implementation,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        uint256 salt
+    );
+
+    function createAccount(
+        address implementation,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        uint256 seed,
+        bytes calldata initData
+    ) external returns (address);
+
+    function account(
+        address implementation,
+        uint256 chainId,
+        address tokenContract,
+        uint256 tokenId,
+        uint256 salt
+    ) external view returns (address);
+}
+
+interface IERC6551AccountProxy {
+    function implementation() external view returns (address);
+}
+
+interface IERC6551Account {
+    event TransactionExecuted(address indexed target, uint256 indexed value, bytes data);
+
+    receive() external payable;
+
+    function executeCall(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable returns (bytes memory);
+
+    function nftInfo()
+        external
+        view
+        returns (
+            uint256 chainId,
+            address tokenContract,
+            uint256 tokenId
+        );
+
+    function owner() external view returns (address);
+
+    function nonce() external view returns (uint256);
+}
