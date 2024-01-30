30757624ad1393daa70e3a9ee5cc783eaa6c36c1
Net2Dev <info@a3b.io>
2024-01-30 13:38:45 -0800

    Update N2D-ERC6551-Registry.sol

--- contracts/N2D-ERC6551-Registry.sol
+++ contracts/N2D-ERC6551-Registry.sol
@@ -1,5 +1,23 @@
 // SPDX-License-Identifier: MIT
 
+/*
+Follow/Subscribe Youtube, Github, IM, Tiktok
+for more amazing content!!
+@Net2Dev
+███╗░░██╗███████╗████████╗██████╗░██████╗░███████╗██╗░░░██╗
+████╗░██║██╔════╝╚══██╔══╝╚════██╗██╔══██╗██╔════╝██║░░░██║
+██╔██╗██║█████╗░░░░░██║░░░░░███╔═╝██║░░██║█████╗░░╚██╗░██╔╝
+██║╚████║██╔══╝░░░░░██║░░░██╔══╝░░██║░░██║██╔══╝░░░╚████╔╝░
+██║░╚███║███████╗░░░██║░░░███████╗██████╔╝███████╗░░╚██╔╝░░
+╚═╝░░╚══╝╚══════╝░░░╚═╝░░░╚══════╝╚═════╝░╚══════╝░░░╚═╝░░░
+THIS CONTRACT IS AVAILABLE FOR EDUCATIONAL 
+PURPOSES ONLY. YOU ARE SOLELY REPONSIBLE 
+FOR ITS USE. I AM NOT RESPONSIBLE FOR ANY
+OTHER USE. THIS IS TRAINING/EDUCATIONAL
+MATERIAL. ONLY USE IT IF YOU AGREE TO THE
+TERMS SPECIFIED ABOVE.
+*/
+
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/utils/Create2.sol";
